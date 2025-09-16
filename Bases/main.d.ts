79e4d4546cab37a42ea8abacf5b3c99ca03073e2
SamuelZambrana <samu-1994@hotmail.es>
2025-09-16 10:31:34 +0200

    ADD: Interfaces

--- Bases/main.d.ts
+++ Bases/main.d.ts
@@ -1,2 +1,5 @@
-declare module "basica" { }
+declare module "basicas" { }
+declare module "clases" { }
+declare module "complejas" { }
+declare module "funciones" { }
 //# sourceMappingURL=main.d.ts.map
--- Bases/main.js
+++ Bases/main.js
@@ -1,33 +1,80 @@
-System.register("basica", [], function (exports_1, context_1) {
+System.register("basicas", [], function (exports_1, context_1) {
     "use strict";
     var __moduleName = context_1 && context_1.id;
     return {
         setters: [],
         execute: function () {
             (() => {
-                class Avenger {
-                    name;
+                // Bases/interfaces/basicas.ts
+                const superheroe = {
+                    nombre: "Dr. Strange",
+                    artesMarciales: ["Karate", "Aikido", "Wing Chun", "Jiu-Jitsu"]
+                };
+                const { nombre, artesMarciales } = superheroe;
+                console.log(nombre);
+                console.log(artesMarciales.join(", "));
+            })();
+        }
+    };
+});
+System.register("clases", [], function (exports_2, context_2) {
+    "use strict";
+    var __moduleName = context_2 && context_2.id;
+    return {
+        setters: [],
+        execute: function () {
+            (() => {
+                class Mutant {
+                    nombre;
                     realName;
-                    isAlive;
-                    age;
-                    powers;
-                    static avgAge = 35;
-                    constructor(name, realName, isAlive = true, age, powers = []) {
-                        this.name = name;
+                    constructor(nombre, realName) {
+                        this.nombre = nombre;
                         this.realName = realName;
-                        this.isAlive = isAlive;
-                        this.age = age;
-                        this.powers = powers;
-                        this.name = name;
-                        this.realName = realName;
-                        this.isAlive = isAlive;
-                        this.age = age;
-                        this.powers = powers;
+                    }
+                    mutandPower(id) {
+                        return this.nombre + ' ' + id;
                     }
                 }
-                const antman = new Avenger('Antman', 'Scott Lang');
-                console.log(antman);
-                console.log(Avenger.avgAge);
+                const wolverine = new Mutant('Wolverine', 'Logan');
+                const charles = new Mutant('Profesor X', 'Charles Xavier');
+                console.log(wolverine.mutandPower(1));
+                console.log(charles.mutandPower(2));
+            });
+        }
+    };
+});
+System.register("complejas", [], function (exports_3, context_3) {
+    "use strict";
+    var __moduleName = context_3 && context_3.id;
+    return {
+        setters: [],
+        execute: function () {
+            (() => {
+                const client = {
+                    name: 'Alejandro',
+                    age: 23,
+                    address: {
+                        id: 123456,
+                        zip: '12345',
+                        city: 'New York'
+                    },
+                    getFullAddress(id) {
+                        return this.address?.city || '';
+                    }
+                };
+            });
+        }
+    };
+});
+System.register("funciones", [], function (exports_4, context_4) {
+    "use strict";
+    var __moduleName = context_4 && context_4.id;
+    return {
+        setters: [],
+        execute: function () {
+            (() => {
+                const addNumbers = (a, b) => a + b;
+                console.log(addNumbers(1, 2));
             });
         }
     };
